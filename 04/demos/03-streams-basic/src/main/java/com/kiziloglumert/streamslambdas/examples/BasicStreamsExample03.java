5f6f669122c6e0cb39b9d738132668a8855ad8ce
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-10-14 19:18:00 +0300

    functional

--- 04/demos/03-streams-basic/src/main/java/com/kiziloglumert/streamslambdas/examples/BasicStreamsExample03.java
+++ 04/demos/03-streams-basic/src/main/java/com/kiziloglumert/streamslambdas/examples/BasicStreamsExample03.java
@@ -14,17 +14,23 @@ public class BasicStreamsExample03 {
     public static void main(String[] args) {
         List<Product> products = ExampleData.getProducts();
 
+
+
         // The filter() intermediate operation filters elements from the stream
         products.stream()
                 .filter(product -> product.getCategory() == Category.FOOD)   //the filter category
                 .forEach(System.out::println);
+
+
 
         // The map() intermediate operation does a one-to-one transformation on each element
         products.stream()
                 .map(product -> String.format("The price of %s is $ %.2f", product.getName(), product.getPrice()))
                 .forEach(System.out::println);
 
-        // The flatMap() intermediate operation does a one-to-N transformation on each element
+
+
+        // The flatMap() intermediate operation does a one-to-N transformation on each element 1-N
         // The function passed to flatMap() must return a Stream that contains the output elements
         // The streams returned by the calls to the function are "flatted" into a single output stream
         Pattern spaces = Pattern.compile("\\s+");
